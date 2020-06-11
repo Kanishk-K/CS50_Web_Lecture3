68829fcdfad3349bcea06a6f55bc88cb7b2c3fec
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-06-11 14:41:16 -0500

    Added some model code, beginning JS.

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()
+    toppings = models.IntegerField()
+    size = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name} is a {self.size} pizza with {self.toppings} toppings which costs ${self.price}."
+
+class PizzaToppings(models.Model):
+    name = models.CharField(max_length=64)
+
+class Sub(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()
+    size = models.CharField(max_length=64)
+
+class SubToppings(models.Model):
+    name = models.CharField(max_length=64)
+    size = models.CharField(max_length=64)
+    price = models.FloatField()
+
+class Pasta(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()
+
+class Salad(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField()
+
+class DinnerPlatters(models.Model):
+    name = models.CharField(max_length=64)
+    size = models.CharField(max_length=64)
+    price = models.FloatField()
+
---
+++ orders/static/scripts/layout.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const PizzaButton = document.querySelector(".Pizza");
+    const SubButton = document.querySelector(".Sub");
+    const PastaButton = document.querySelector(".Pasta");
+    const SaladButton = document.querySelector(".Salad");
+    const PlatterButton = document.querySelecor(".Platter");
+})
